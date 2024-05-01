a3be034778e7060853444335a4b09e66d2a623ef
Tibor <112723857+Tibor0106@users.noreply.github.com>
2024-05-01 08:31:40 +0200

    Add project files.

---
+++ dotnet C#-web-api-base/Controllers/BaseController.cs
@@ -0,0 +1,70 @@
+﻿using dotnet_C__web_api_base.Database;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_C__web_api_base.Controllers
+{
+    [Route("[controller]Requets")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public BaseController(AppDbContext appDbcontext)
+        {
+            _appDbContext = appDbcontext;
+        }
+
+        [HttpPut]
+        [Route("method-put")]
+        public async Task<IActionResult> MethodPut()
+        {    
+            return Ok("Sikeres kérés");
+        }
+
+        [HttpPost]
+        [Route("method-post")]
+        public async Task<IActionResult> MethodPost()
+        {
+            return Ok("Sikeres kérés");
+        }
+
+        //pl: adat lekéresekor használatos
+        [HttpGet]
+        [Route("method-get")]
+        public async Task<IActionResult> MethodGet()
+        {
+            return Ok("Sikeres kérés");
+        }
+
+        //pl: adat törlésekor használatos
+        [HttpDelete]
+        [Route("method-delete")]
+        public async Task<IActionResult> MethodDelete()
+        {
+            return Ok("Sikeres kérés");
+        }
+
+        //pl: adat szerkesztéskor használatos
+        [HttpPatch]
+        [Route("method-patch")]
+        public async Task<IActionResult> MethodPatch()
+        {
+            return Ok("Sikeres kérés");
+        }
+
+        [HttpOptions]
+        [Route("method-options")]
+        public async Task<IActionResult> MethodOptions()
+        {
+            return Ok("Sikeres kérés");
+        }
+
+        //pl: fejléc küldésekor
+        [HttpHead]
+        [Route("method-head")]
+        public async Task<IActionResult> MethodHead()
+        {
+            return Ok("Sikeres kérés");
+        }
+    }
+}
---
+++ dotnet C#-web-api-base/Database/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_C__web_api_base.Database
+{
+    public class AppDbContext : DbContext
+    {
+       // public DbSet<-TYPE-> -variable name- { get; set; }
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
